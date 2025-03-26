7787bca49b102511d9c6a5669ee3ff75c56223eb
Lorenz Gaertner <lorenz.gaertner@physik.uni-muenchen.de>
2025-03-26 14:46:10 +0100

    increase readability

--- mcmc.py
+++ mcmc.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def metropolis_hastings(twice_nl_posterior, n_samples, theta_init, proposal_std=0.1):
+def metropolis_hastings(twice_neg_log_posterior, n_samples, theta_init, proposal_std=0.1):
     """Generate samples from a posterior distribution, using the Metropolis-Hastings algorithm.
 
     Args:
-        twice_nl_posterior (callable): -2*ln(p(theta|x)), takes theta (array) as input.
+        twice_neg_log_posterior (callable): -2*ln(p(theta|x)), takes theta (array) as input.
         n_samples (int): Number of samples per chain.
         theta_init (List[float]): Initial starting point in parameter space.
         proposal_std (float, List[float]): Standard deviation of Gaussian proposal distribution.
@@ -16,16 +16,17 @@ def metropolis_hastings(twice_nl_posterior, n_samples, theta_init, proposal_std=
     current = theta_init
 
     for _ in range(n_samples):
-        # Propose new candidate
+        # Propose new candidate from na normal proposal distribution
         proposal = np.random.normal(loc=current, scale=proposal_std, size=2)
 
-        # Compute acceptance ratio
-        nlog_current = twice_nl_posterior(current)
-        nlog_proposal = twice_nl_posterior(proposal)
-        nlog_alpha = max(0, nlog_proposal - nlog_current)
+        # Compute acceptance ratio (in neg log space)
+        twice_neg_log_current = twice_neg_log_posterior(current)
+        twice_neg_log_proposal = twice_neg_log_posterior(proposal)
+        twice_neg_log_alpha = max(0, twice_neg_log_proposal - twice_neg_log_current)
 
         # Accept/reject step
-        if -2*np.log(np.random.rand()) > nlog_alpha:
+        u = np.random.rand()
+        if -2*np.log(u) > twice_neg_log_alpha:
             current = proposal  # Accept proposal
 
         samples.append(current)
